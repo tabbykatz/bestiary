13ce74be1acaf6a6302acc1a168232d1c5534d19
tabbykatz <tomelay@gmail.com>
2021-09-16 12:32:21 -0700

    add broken time in addSighting

--- app/src/Sightings.js
+++ app/src/Sightings.js
@@ -10,23 +10,77 @@ const Sightings = () => {
     getSightings();
   }, []);
 
+  const addSighting = (sighting) =>
+    apiClient.addSighting(sighting).then(getSightings);
+
+  const deleteSighting = (id) => {
+    console.log(id);
+    apiClient.deleteSighting(id).then(getSightings);
+  };
+
+  const AddSightingForm = ({ addSighting }) => {
+    const onSubmit = (event) => {
+      const form = event.currentTarget;
+      const added = new Date().toISOString();
+      const {
+        specimen_id: { value: specimen_id },
+        healthy: { value: healthy },
+        email: { value: email },
+        location: { value: location },
+      } = form.elements;
+
+      event.preventDefault();
+      console.log(added, specimen_id, healthy, email, location);
+      addSighting({ added, specimen_id, healthy, email, location });
+      form.reset();
+    };
+
+    return (
+      <form {...{ onSubmit }}>
+        <label>
+          Specimen ID
+          <input name="specimen_id" required />
+        </label>
+        <label>
+          Healthy?
+          <input name="healthy" required />
+        </label>
+        <label>
+          Email
+          <input name="email" type="email" required />
+        </label>
+        <label>
+          Location
+          <input name="location" required />
+        </label>
+        <button>Add new sighting</button>
+      </form>
+    );
+  };
+
   return (
-    <ul>
-      {sightings.map(
-        ({
-          sighting_id,
-          time_sighted,
-          specimen_id,
-          healthy,
-          email,
-          location,
-        }) => (
-          <li key={sighting_id}>
-            {time_sighted} : {location}
-          </li>
-        ),
-      )}
-    </ul>
+    <>
+      <ul>
+        {sightings.map(
+          ({
+            sighting_id,
+            time_sighted,
+            specimen_id,
+            healthy,
+            email,
+            location,
+          }) => (
+            <li key={sighting_id}>
+              {time_sighted}: {location}
+              <button onClick={() => deleteSighting(sighting_id)}>
+                Delete
+              </button>
+            </li>
+          ),
+        )}
+      </ul>
+      <AddSightingForm addSighting={addSighting} />
+    </>
   );
 };
 
